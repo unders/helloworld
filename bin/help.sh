1d45d5de53a69a812a3451b4ec60e4b5925e8ac5
Anders Törnqvist <anders.tornqvist@gmail.com>
2017-03-24 09:04:30 +0100

    Update help messages in Makefile

--- bin/help.sh
+++ bin/help.sh
@@ -7,7 +7,7 @@ main() {
 	echo "    - and deploy them to Google Container Engine"
 	echo ""
 	echo "Commands:"
-	echo "    make bootstrap     # check required dependencies"
+	echo "    make bootstrap     # checks required dependencies"
 	echo "    make xxxxx         # next"
 	echo ""
 	echo "Tutorial"
