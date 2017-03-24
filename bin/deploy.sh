078917e01aa01b70b330e6d8f964bfb67c8b4817
Anders Törnqvist <anders.tornqvist@gmail.com>
2017-03-24 16:38:30 +0100

    WIP: Deploy stuff

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,15 @@ deploy_nginx() {
     echo ""
     echo "## Deploy nginx"
     echo ""
+    echo "    kubectl run nginx --image=nginx:1.10.0"
+    echo "    kubectl get pods"
+    echo "    gcloud compute instances list"
+    echo "    kubectl get services"
+    echo "    kubectl expose deployment nginx --port 80 --type LoadBalancer"
+    echo "    kubectl get services"
+    echo "    curl -I 35.187.5.84"
+    echo "    curl 35.187.5.84"
+    echo ""
 
 }
 
